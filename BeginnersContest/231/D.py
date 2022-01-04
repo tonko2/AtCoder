5324396840ba47a4ec268bf9f52db6e2b2fd7496
tonko2 <10cherry07@gmail.com>
2022-01-05 07:20:21 +0900

    Solved D

--- BeginnersContest/231/D.py
+++ BeginnersContest/231/D.py
@@ -1,23 +1,54 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+sys.setrecursionlimit(10**6)
+
 N, M = map(int, input().split())
 
-hashmap = {}
-ok = True
+par = [i for i in range(N)]
+def find(x):
+    if par[x] == x:
+        return x
+    else:
+        par[x] = find(par[x])
+        return par[x]
+
+def same(x, y):
+    return find(x) == find(y)
+
+def unite(x, y):
+    x = find(x)
+    y = find(y)
+    if x == y:
+        return 0
+    par[x] = y
+
+cnt = [0] * (N + 1)
+
+flag = True
 for _ in range(M):
     A, B = map(int, input().split())
-    a = hashmap.get(A, 0)
-    b = hashmap.get(B, 0)
-    hashmap[A] = a + 1
-    hashmap[B] = b + 1
+    A, B = A-1, B-1
+    cnt[A] += 1
+    cnt[B] += 1
+    unite(A, B)
 
-    if a == 1:
-        hashmap[B] = 2
-    if b == 1:
-        hashmap[A] = 2
+    if cnt[A] > 2 or cnt[B] > 2:
+        flag = False
 
-    if a > 1 or b > 1:
-        ok = False
+if not flag:
+    print('No')
+    exit()
 
-print("Yes" if ok else "No")
+flag = dict()
+
+for i in range(N):
+    p = find(i)
+    if cnt[i] < 2:
+        flag[p] = True
+    elif not p in flag:
+        flag[p] = False
+if False in flag.values():
+    print('No')
+else:
+    print('Yes')
