5121ba57fc4f11f424d5b6ac28a888441e0fa350
Yuya <10cherry07@gmail.com>
2022-01-31 21:40:52 +0900

    Solved C

--- BeginnerContest/230/C.py
+++ BeginnerContest/230/C.py
@@ -1,3 +1,24 @@
-N, A, B = map(int, input().split())
-P, Q, R, S = map(int, input().split())
+import sys
 
+sys.setrecursionlimit(10 ** 6)
+stdin = sys.stdin
+
+def main():
+    ni = lambda: int(ns())
+    na = lambda: list(map(int, stdin.readline().split()))
+    ns = lambda: stdin.readline().strip()
+
+    N, A, B = na()
+    P, Q, R, S = na()
+
+    for i in range(P, Q + 1):
+        line = []
+        for j in range(R, S + 1):
+            if i - j == A - B or i + j == A + B:
+                line.append('#')
+            else:
+                line.append('.')
+        print("".join(line))
+
+if __name__ == "__main__":
+    main()
