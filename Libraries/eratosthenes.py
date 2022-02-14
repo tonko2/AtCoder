82b61524854b3727385a90f5b068efa927eaa7c5
Yuya <10cherry07@gmail.com>
2022-02-15 08:17:49 +0900

    Updated eratosthenes

--- Libraries/eratosthenes.py
+++ Libraries/eratosthenes.py
@@ -1,12 +1,19 @@
-N = int(input())
-prime = [ True ] * (N + 1)
+def eratosthenes(N):
+    prime = [ True ] * (N + 1)
+    LIMIT = int(N ** 0.5)
+    for i in range(2, LIMIT + 1):
+        if prime[i] == True:
+            for j in range(2 * i, N + 1, i):
+                prime[j] = False
+    return prime
 
-LIMIT = int(N ** 0.5)
-for i in range(2, LIMIT + 1):
-    if prime[i] == True:
-        for j in range(2 * i, N + 1, i):
-            prime[j] = False
+def main():
+    N = int(input())
+    prime = eratosthenes(N)
 
-for i in range(2, N + 1):
-    if prime[i] == True:
-        print(i)
+    for i in range(2, N + 1):
+        if prime[i] == True:
+            print(i)
+
+if __name__ == '__main__':
+    main()
