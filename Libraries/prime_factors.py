306ac809f9841aa2d557d31e81680f16593bb738
Yuya Kusaka <10cherry07@gmail.com>
2022-04-09 10:55:31 +0900

    Updated prime_factors

--- Libraries/prime_factors.py
+++ Libraries/prime_factors.py
@@ -1,14 +1,12 @@
 import math
 
-def prime_factors(N):
-    rem = N
+def prime_factors(n):
+    rem = n
     res = []
-
-    for i in range(2, int(math.sqrt(N)) + 1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         while rem % i == 0:
-            rem /= i
+            rem //= i
             res.append(i)
-
     if rem != 1:
         res.append(rem)
     return res
