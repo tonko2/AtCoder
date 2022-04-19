7bb3aa81fedba44a92c6bf0b945a76c8e4cd98f3
Yuya Kusaka <10cherry07@gmail.com>
2022-04-19 20:56:00 +0900

    Solved typical start4 problems

--- typical90/034/main.py
+++ typical90/034/main.py
@@ -11,3 +11,23 @@ ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+N, K = na()
+A = na()
+
+cur = 0
+cnt = 0
+ans = 0
+d = defaultdict(int)
+for i in range(N):
+    while cur < N:
+        if d[A[cur]] == 0 and cnt == K:
+            break
+        if d[A[cur]] == 0:
+            cnt += 1
+        d[A[cur]] += 1
+        cur += 1
+    ans = max(ans, cur - i)
+    if d[A[i]] == 1:
+        cnt -= 1
+    d[A[i]] -= 1
+print(ans)
--- typical90/042/main.py
+++ typical90/042/main.py
@@ -6,8 +6,20 @@ sys.setrecursionlimit(10 ** 6)
 stdin = sys.stdin
 
 INF = float('inf')
+MOD = 10 ** 9 + 7
 
 ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+K = ni()
+if K % 9 != 0:
+    print(0)
+else:
+    dp = [0] * (K + 1)
+    dp[0] = 1
+    for i in range(1, K + 1):
+        j = min(i, 9)
+        for j in range(1, j + 1):
+            dp[i] = (dp[i] + dp[i - j]) % MOD
+    print(dp[K])
