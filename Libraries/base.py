e4b46ddbd4523cfb2ee61428f671d8d0b395532a
Yuya Kusaka <10cherry07@gmail.com>
2022-05-09 08:18:21 +0900

    Updated base library

--- Libraries/base.py
+++ Libraries/base.py
@@ -6,12 +6,17 @@ def base_10_to_n(x, n):
     res.reverse()
     return "".join(res)
 
+def base_n_to_10(x, n):
+    return int(str(x), n)
+
 def main():
     x = 17
     print(base_10_to_n(x, 9))
 
     str_x = '21'
     print(int(str_x, 8))
+
+    print(base_n_to_10(18, 9))
 
 if __name__ == "__main__":
     main()
