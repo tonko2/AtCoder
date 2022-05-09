f9d5a799caf48b6a08867d0de509bb0efc7bced5
Yuya Kusaka <10cherry07@gmail.com>
2022-05-09 16:30:15 +0900

    Solved typical star 5 problems

--- typical90/037/main.py
+++ typical90/037/main.py
@@ -11,3 +11,86 @@ ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+class SegTree:
+    """
+    Segment Tree
+    """
+
+    def __init__(self, init_val, segfunc=min, ide_ele=float('inf')):
+        """
+        初期化
+
+        init_val: 配列の初期値
+        """
+        n = len(init_val)
+        self.segfunc = segfunc
+        self.ide_ele = ide_ele
+        self.num = 1 << (n - 1).bit_length()
+        self.tree = [ide_ele] * 2 * self.num
+        # 配列の値を葉にセット
+        for i in range(n):
+            self.tree[self.num + i] = init_val[i]
+        # 構築していく
+        for i in range(self.num - 1, 0, -1):
+            self.tree[i] = segfunc(self.tree[2 * i], self.tree[2 * i + 1])
+
+    def update(self, k, x):
+        """
+        k番目の値をxに更新
+
+        k: index(0-index)
+        x: update value
+        """
+        k += self.num
+        self.tree[k] = x
+        while k > 1:
+            self.tree[k >> 1] = self.segfunc(self.tree[k], self.tree[k ^ 1])
+            k >>= 1
+
+    def query(self, l, r):
+        """
+        [l, r)のsegfuncしたものを得る
+
+        l: index(0-index)
+        r: index(0-index)
+        """
+        res = self.ide_ele
+
+        l += self.num
+        r += self.num
+        while l < r:
+            if l & 1:
+                res = self.segfunc(res, self.tree[l])
+                l += 1
+            if r & 1:
+                res = self.segfunc(res, self.tree[r - 1])
+            l >>= 1
+            r >>= 1
+        return res
+
+W, N = na()
+L = [0] * N
+R = [0] * N
+V = [0] * N
+for i in range(N):
+    L[i], R[i], V[i] = na()
+
+def segfunc(x, y):
+    return max(x, y)
+ide_ele = -1
+
+seg = SegTree([-1] * (W + 2), segfunc=segfunc, ide_ele=ide_ele)
+seg.update(0, 0)
+
+for a in range(N):
+    l, r, v = L[a], R[a], V[a]
+    for i in range(W + 1)[::-1]:
+        if i - l < 0:
+            continue
+        now = seg.query(i, i + 1)
+        x = seg.query(max(0, i - r), i - l + 1)
+        if x == -1:
+            continue
+        if now < x + v:
+            seg.update(i, x + v)
+print(seg.query(W, W + 1))
--- typical90/039/main.py
+++ typical90/039/main.py
@@ -11,3 +11,31 @@ ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+def dfs(pos, pre):
+    dp[pos] = 1
+    for v in G[pos]:
+        if v == pre:
+            continue
+        dfs(v, pos)
+        dp[pos] += dp[v]
+
+N = ni()
+A = [0] * (N - 1)
+B = [0] * (N - 1)
+dp = [0] * (N)
+G = [[] for _ in range(N)]
+for i in range(N - 1):
+    a, b = na()
+    a -= 1
+    b -= 1
+    A[i] = a
+    B[i] = b
+    G[A[i]].append(B[i])
+    G[B[i]].append(A[i])
+
+dfs(0, -1)
+ans = 0
+for i in range(N - 1):
+    r = min(dp[A[i]], dp[B[i]])
+    ans += r * (N - r)
+print(ans)
