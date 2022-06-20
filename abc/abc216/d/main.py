54025229df2c881e1464011d16e15dcd4b835e4a
yuya kusaka <10cherry07@gmail.com>
2022-06-20 09:01:51 +0900

    Solved topological sort problems

--- abc/abc216/d/main.py
+++ abc/abc216/d/main.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 from collections import defaultdict
 
 sys.setrecursionlimit(10 ** 6)
@@ -10,3 +11,31 @@ ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+def topological_sort(inc, out):
+    # 入次数が0のノード
+    q = [i for i, c in enumerate(inc) if c == 0]
+    res = []
+    while q:
+        v = heapq.heappop(q)
+        res.append(v)
+        for j in out[v]:
+            inc[j] -= 1
+            if inc[j] == 0:
+                heapq.heappush(q, j)
+    return res
+
+N, M = na()
+inc = [0] * N
+out = [[] for _ in range(N)]
+
+for _ in range(M):
+    _ = ni()
+    A = na()
+    for i in range(len(A) - 1):
+        inc[A[i + 1] - 1] += 1
+        out[A[i] - 1].append(A[i + 1] - 1)
+ans = topological_sort(inc, out)
+if len(ans) == N:
+    print("Yes")
+else:
+    print("No")
--- abc/abc245/f/main.py
+++ abc/abc245/f/main.py
@@ -1,4 +1,5 @@
 import sys
+import heapq
 from collections import defaultdict, deque
 
 sys.setrecursionlimit(10 ** 6)
@@ -10,3 +11,23 @@ ni = lambda: int(ns())
 na = lambda: list(map(int, stdin.readline().split()))
 ns = lambda: stdin.readline().strip()
 
+N, M = na()
+inc = [0] * N
+out = [[] for _ in range(N)]
+for _ in range(M):
+    a, b = na()
+    a -= 1
+    b -= 1
+    inc[a] += 1
+    out[b].append(a)
+ans = N
+q = [i for i, c in enumerate(inc) if c == 0]
+while q:
+    ans -= 1
+    v = heapq.heappop(q)
+    s = len(out[v])
+    for i in range(s):
+        inc[out[v][i]] -= 1
+        if inc[out[v][i]] == 0:
+            heapq.heappush(q, out[v][i])
+print(ans)
